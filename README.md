9ea57759306aa702017277f2d2b97b3883739754
IRonnyc <nicolas.lagemann@gmail.com>
2023-06-27 00:36:55 +0200

    Documentation and minor refactoring

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # NoShortsJustWatch
 Simply redirects youtube.com/shorts/ to youtube.com/watch/.
 
-It uses a simple JavaScript one-liner, no fancy nonsense :)
+It uses a simple JavaScript one-liner, no fancy nonsense. It's held simple, in the hopes that it's easy to verify it only does, what it says. :)
+
+(Sadly this makes it kind of difficult to properly test it 😅)
--- noshorts.js
+++ noshorts.js
@@ -1,1 +1,18 @@
-window.location.replace(window.location.toString().replace("shorts", "watch"))
+/**
+ * Replaces '.com/shorts' with '.com/watch' in the passed string.
+ * 
+ * @param {String} path path to replace shorts with watch in.
+ * @returns the updated path
+ */
+function replaceShorts(path) {
+    return path.replace(".com/shorts/", ".com/watch?v=");
+}
+
+/**
+ * Replaces '.com/shorts/' with '.com/watch?v=' in the current url and redirects to that.
+ */
+function redirect() {
+    window.location.replace(replaceShorts(window.location.toString()));
+}
+
+redirect();
